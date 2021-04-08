fa2a31e3c552856fb78f803ad64601ec8c303da3
KKKPJSKEY <KKKPJSKEY@outlook.com>
2021-04-09 02:31:31 +0800

    project rename

--- app/src/main/java/cn/edu/hubu/lhy/Weixin_MD/FriFragment.java
+++ app/src/main/java/cn/edu/hubu/lhy/Weixin_MD/FriFragment.java
@@ -1,4 +1,4 @@
-package cn.edu.hubu.lhy.bottom_navigation;
+package cn.edu.hubu.lhy.Weixin_MD;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,10 +9,10 @@ import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SetFragment#newInstance} factory method to
+ * Use the {@link FriFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SetFragment extends Fragment {
+public class FriFragment extends Fragment {
 
 //    // TODO: Rename parameter arguments, choose names that match
 //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,7 +23,7 @@ public class SetFragment extends Fragment {
 //    private String mParam1;
 //    private String mParam2;
 
-//    public MesFragment() {
+//    public FriFragment() {
         // Required empty public constructor
 //    }
 
@@ -58,6 +58,6 @@ public class SetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_set, container, false);
+        return inflater.inflate(R.layout.fragment_fri, container, false);
     }
 }
